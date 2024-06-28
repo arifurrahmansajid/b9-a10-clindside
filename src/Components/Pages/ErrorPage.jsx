7ba909bb4a1e93b08a6fc064cac6cf464bb54c51
arifurrahmansajid <iamnobody3456@gmail.com>
2024-06-28 12:33:10 +0600

    update erroe page

--- src/Components/Pages/ErrorPage.jsx
+++ src/Components/Pages/ErrorPage.jsx
@@ -3,7 +3,7 @@
 const ErrorPage = () => {
     return (
         <div>
-            <h2 className="text-2xl font-bold text-center justify-center my-32 ">Error 4O4</h2>
+            <h2 className="text-2xl font-bold text-center justify-center my-32 "><img src="	https://c.tenor.com/IHdlTRsmcS4AAAAC/404.gif" alt="" /></h2>
         </div>
     );
 };
